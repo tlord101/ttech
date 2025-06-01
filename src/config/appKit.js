d0c768dc3e24a530958a74a5b27f75d66cd7348d
tlord101 <kook28515@gmail.com>
2025-06-01 17:25:48 +0100

    Update appKit.js

--- src/config/appKit.js
+++ src/config/appKit.js
@@ -2,7 +2,7 @@ import { arbitrum, mainnet, optimism, polygon, sepolia } from '@reown/appkit/net
 import { createAppKit } from '@reown/appkit'
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
 
-const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+const projectId = "fd6b27758d54dc8db988468aaa2c07db" // this is a public projectId only to use on localhost
 if (!projectId) {
   throw new Error('VITE_PROJECT_ID is not set')
 }
@@ -11,7 +11,7 @@ export const appKit = createAppKit({
   adapters: [new EthersAdapter()],
   networks: [arbitrum, mainnet, optimism, polygon, sepolia],
   projectId,
-  themeMode: 'light',
+  themeMode: 'dark',
   themeVariables: {
     '--w3m-accent': '#000000',
   },
